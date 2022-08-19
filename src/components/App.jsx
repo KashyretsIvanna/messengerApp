2be0187bad5d4c9d531c88a8305d0daae983c88f
ZadAndRot <kasirecivanna@gmail.com>
2022-08-19 09:38:36 +0300

    feat: :sparkles:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,25 @@
 import Contacts from './Contacts';
 import Chat from './Chat';
-import { useEffect } from 'react';
-import jwt_decode from 'jwt-decode';
+// import { useEffect } from 'react';
+// import jwt_decode from 'jwt-decode';
 
 export const App = () => {
-  function handleCallbackResponse(response) {
-    console.log(jwt_decode(response.credential));
-  }
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id:
-        '71848933982-fg672e4995svgotpfiko6moelsbjooc7.apps.googleusercontent.com',
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.renderButton(document.getElementById('signin'), {
-      theme: 'outline',
-      size: 'large',
-    });
-    // google.accounts.id.propmpt();
-  }, []);
+  // function handleCredentialResponse(response) {
+  //   console.log(jwt_decode(response.credential));
+  // }
+  
+  // useEffect(() => {
+  //   google.accounts.id.initialize({
+  //     client_id:
+  //       '71848933982-fg672e4995svgotpfiko6moelsbjooc7.apps.googleusercontent.com',
+  //     callback: handleCallbackResponse,
+  //   });
+  //   google.accounts.id.renderButton(document.getElementById('signin'), {
+  //     theme: 'outline',
+  //     size: 'large',
+  //   });
+  //   // google.accounts.id.propmpt();
+  // }, []);
   return (
     <div
       style={{
